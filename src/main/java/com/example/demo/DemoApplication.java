ac9df62889fe0d7ef378f89bd21839cc8eb5e464
Akalin <a.f.akalin@gmail.com>
2020-03-09 16:51:34 +0100

    initial commit for deploying on heroku

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	// dakika 35 den itibaren devam edebilirsin
+	//post  ve get oluyor
+
+}
